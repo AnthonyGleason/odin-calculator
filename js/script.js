0b05b095e4526fe1ee69108e5a768bc7a9bb97de
Anthony Infortunio <ainfortunio1@gmail.com>
2022-06-10 18:48:06 -0400

    Add performCalculations() function, allow multiple operations
    This commit creates the performCalculations() function and allows for
    the user to string together multiple calculations at once.
    performCalculations() works by using commas around operators in
    displayValue which we can use to split the string into an array
    around the operators. performCalculations() will then check to see
    if there are any operators left in the displayValue. This will let the
    function know before displaying the total if there are any more
    calculations to perform. This commit also changes the functionality of
    the = button. Instead of displaying = in the calculator's display the
    display will now be cleared and showed only the total of the
    calculations.

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ let divide = function (firstNum,secondNum){
     total=firstNum/secondNum;
     return total;
 }
-
 let operate = function (operator,firstNum,secondNum){
     switch (operator){
         case '+':
@@ -38,19 +37,48 @@ let operate = function (operator,firstNum,secondNum){
 }
 let displayInput = function (userInput){
     const calcDisplay=document.querySelector('.screen');
-    if (userInput.textContent=='clr')
-    {
+    if (typeof userInput=="number"){
+        calcDisplay.textContent="";
+        calcDisplay.textContent+=(userInput);
+    }else if (userInput.textContent=='clr'){
         calcDisplay.textContent="";
     }else{
         calcDisplay.textContent+=userInput.textContent;
     }
+    return calcDisplay.textContent;
 }
-
+let performCalculations = function (displayValue){
+    let displayValueModified=displayValue
+    let displayValueArray=[];
+    let total =0;
+    let numa=0;
+    let numb=0;
+    //seperate operators for array split
+    displayValueModified=displayValueModified.replaceAll("+",",+,")
+    displayValueModified=displayValueModified.replaceAll("-",",-,")
+    displayValueModified=displayValueModified.replaceAll("*",",*,")
+    displayValueModified=displayValueModified.replaceAll("/",",/,")
+    displayValueArray=displayValueModified.split(',');
+    for (let i=0;i<=displayValueArray.length;i++){
+        if (displayValueArray[i]=='+'||displayValueArray[i]=='-'||displayValueArray[i]=='*'||displayValueArray[i]=='/'){
+            total=operate(displayValueArray[i],parseInt(displayValueArray[i-1]),parseInt(displayValueArray[i+1]));
+            displayValueArray.splice(i+2,0,total);
+            displayValueArray.splice(i-1,3);
+            i--;
+        }
+    }
+    displayInput(total);
+}
 const buttonContainer = document.querySelector('.button-container');
 const allButtons=buttonContainer.querySelectorAll('button');
+let displayValue="";
 allButtons.forEach((button)=>{
     button.addEventListener('click',()=>{
-        displayInput(button);
+        if (button.textContent=="="){
+            performCalculations(displayValue);
+        }else{
+            displayValue=displayInput(button);
+        }
     })
 })
 //send total as argument firstNum
